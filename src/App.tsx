7b9ca4cf1f7a0b50cc2af5c45492318d1efb8c2b
nagi-moriyama-lvgs <nagi.moriyama@leverages.jp>
2021-06-30 19:35:18 +0900

    作業途中

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import Square from "./components/atoms/Square";
 import NineSquares from "./components/molecules/NineSquares";
 
 const App = () => {
   return (
     <>
       <h1>hello world</h1>
-      <Square number={1} />
       <NineSquares />
     </>
   );
--- src/components/molecules/NineSquares.tsx
+++ src/components/molecules/NineSquares.tsx
@@ -4,12 +4,38 @@ import Square from "components/atoms/Square";
 
 type BaseNineSquaresProps = {
   className?: string;
+  squareStructure?: Array<number>;
+  defaultValues?: Array<Array<number>>;
 };
 
-const BaseNineSquares: React.FC<BaseNineSquaresProps> = ({ className }) => {
+const BaseNineSquares: React.FC<BaseNineSquaresProps> = ({
+  className,
+  squareStructure = [3, 3],
+  defaultValues = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+  ],
+}) => {
+  //   const Squares: Array<Array<JSX.Element>> = [];
+  //   for (let i = 0; i < squareStructure[1]; i++) {
+  //     const SquaresRow: Array<JSX.Element> = [];
+  //     let SquaresRowElement: JSX.Element = <></>;
+  //     for (let j = 0; j < squareStructure[0]; j++) {
+  //       SquaresRow.push(<Square number={defaultValues[j][i]} />);
+  //       SquaresRowElement = <div>{SquaresRow}</div>;
+  //     }
+  //     Squares.push(SquaresRow);
+  //   }
+
+  const SquaresRow: Array<JSX.Element> = [];
+  for (let i = 0; i < squareStructure[0]; i++) {
+    SquaresRow.push(<Square number={defaultValues[0][i]} key={i} />);
+  }
+
   return (
     <div className={className}>
-      <Square number={1} />
+      <div className={"flex"}>{SquaresRow}</div>
     </div>
   );
 };
@@ -24,6 +50,9 @@ const NineSquares = styled(BaseNineSquares)`
     align-items: center;
     span {
       font-size: 2rem;
+    }
+    .flex {
+      display: flex;
     }
   }
 `;
